17342c51f3829b965fabcc6405ffd22ee70f2e4f
Michael Lajlev <lajlev@gmail.com>
2019-07-10 15:20:13 +0200

    better link to gmaps

--- scripts/main.src.js
+++ scripts/main.src.js
@@ -51,7 +51,7 @@ $('#sendRegistrationNumber').click(function(e){
 
 function locationValue(){
   if(localStorage.getItem("DahCrdLat")!==null) {
-    return "http://www.google.com/maps/place/" + localStorage.getItem("DahCrdLat") + "," + localStorage.getItem("DahCrdLon")
+    return "http://maps.google.com/maps?q=" + localStorage.getItem("DahCrdLat") + "," + localStorage.getItem("DahCrdLon")
   } else {
     return "Location not available"
   }
